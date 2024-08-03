2387c7b20b16729fb0308422e5007af59dd9b782
Drihmia <drihmia.redouane@gmail.com>
2024-08-03 21:16:06 +0100

    refactor: using Buffer to decode or encode from or to base64 instead of using base64 package

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,3 @@
-import base64 from 'base-64';
 import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
@@ -13,7 +12,7 @@ class AuthController {
     const base64String = authorization.slice(6);
 
     // Decode the base64's string into the original string: "bob@dylan.com:toto1234!".
-    const userData = base64.Buffer.from(base64String, 'base64').toString('utf-8');
+    const userData = Buffer.from(base64String, 'base64').toString('utf-8');
 
     // Get the email and the password variables.
     const [email, password] = userData.split(':');
@@ -79,8 +78,6 @@ class AuthController {
 
     // Get the User's email from database based on the User ID retrieved from Redis.
     const userDoc = await dbClient.findUserById(userId, { password: 0, _id: 0 });
-
-    console.log('userDoc:', userDoc);
 
     // Double check if the User exists, if not send 401 "Unauthorized" error response.
     if (!userDoc) {
--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,4 +1,3 @@
-import base64 from 'base-64';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -8,7 +7,6 @@ class FilesController {
     const redisKey = `auth_${token}`;
 
     const userId = await redisClient.get(redisKey);
-    console.log('usrID:', userId);
     if (!userId) {
       res.status(401).json({ error: 'Unauthorized' });
       return;
@@ -16,7 +14,6 @@ class FilesController {
     const {
       name, type, data, parentId = 0, isPublic = false,
     } = req.body;
-    console.log(name, type, data, parentId, isPublic);
 
     if (name === undefined) {
       res.status(400).json({ error: 'Missing name' });
@@ -71,14 +68,12 @@ class FilesController {
     }
 
     const rootPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-    console.log('rootPath:', rootPath);
 
     const query = {
       name, type, parentId, userId, isPublic, localPath: 'localPath',
     };
 
     const fileId = dbClient.createObject('files', query);
-    console.log('fileId:', fileId);
     res.json({ userId });
   }
 }
