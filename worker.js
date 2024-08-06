8ec0fb90951bc05d8dea55baa417af86867e08fa
Drihmia <drihmia.redouane@gmail.com>
2024-08-06 01:09:26 +0100

    feat: Add file for task 9
      + create a working based on bull for processing jobs 'doing the heaving work'
    
      + creating a queue when the type is image and create job to process by worker
    
      + adjusting the getFile method of FileController class to read data based on size if provided

---
+++ worker.js
@@ -0,0 +1,38 @@
+import Queue from 'bull';
+import imageThumbnail from 'image-thumbnail';
+import { promises as fs } from 'fs';
+import dbClient from './utils/db';
+
+const fileQueue = new Queue('fileQueue');
+
+fileQueue.process(async (job, done) => {
+  const { userId, fileId } = job.data;
+  if (!fileId) return done(new Error('Missing fileId'));
+  if (!userId) return done(new Error('Missing userId'));
+
+  let file;
+  try {
+    file = await dbClient.findFileById(fileId);
+  } catch (_) {
+    done(new Error('File not found'));
+  }
+  if (!file) return done(new Error('File not found'));
+
+  if (String(file.userId) !== userId) return done(new Error('File not found'));
+
+  const location = file.localPath;
+  if (!location) return done(new Error('File not found'));
+
+  const options = [500, 250, 100];
+
+  try {
+    await Promise.all(options.map(async (size) => {
+      const thumbnail = await imageThumbnail(location, { width: size });
+      const thumbnailPath = `${location}_${size}`;
+      await fs.writeFile(thumbnailPath, thumbnail);
+    }));
+    done();
+  } catch (_) {
+    done(new Error('-----'));
+  }
+});
